7f168244485e8d5d24dce17ca352f877f7ae50ec
GransMango <78266855+GransMango@users.noreply.github.com>
2024-05-12 13:04:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,21 @@
+import os
 import geopandas as gpd
+import requests
+
+def download_geojson():
+    url = os.getenv('REGION_DATA_URL')  # The SAS URL for the file
+    response = requests.get(url)
+    os.makedirs('data', exist_ok=True)
+    with open('data/Basisdata_0000_Norge_25833_Fylker_GeoJSON.geojson', 'wb') as f:
+        f.write(response.content)
 
 def load_geojson():
-    geojson_file = 'data/Basisdata_0000_Norge_25833_Fylker_GeoJSON.geojson'
-    gdf = gpd.read_file(geojson_file)
+    data_path = 'data/Basisdata_0000_Norge_25833_Fylker_GeoJSON.geojson'
+    
+    if not os.path.exists(data_path):
+        download_geojson()
+    
+    gdf = gpd.read_file(data_path)
 
     if gdf.crs is None:
         gdf = gdf.to_crs("EPSG:4326")
